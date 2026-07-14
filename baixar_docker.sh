9391aa08da6ce102cd3bb19aee7e54bfe6d09097
jonasbfranco <jonasbfranco@hotmail.com>
2026-07-14 09:53:22 -0300

    :sparkles:: ALteracao arquivos .ps1 e .sh

--- baixar_docker.sh
+++ baixar_docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # apagar arquivos disco externo quando usado no macOS
-docker compose down && echo "Docker finalizados com sucesso" || echo "Erro ao finalizar os containers"   
+docker compose down && echo "Docker finalizados com sucesso" || echo "Erro ao finalizar os containers"
--- delete_files.sh
+++ delete_files.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # apagar arquivos disco externo quando usado no macOS
-find . -name "._*" -delete && echo "Arquivos removidos" || echo "Erro ao remover arquivos"   
+find . -name "._*" -delete && echo "Arquivos removidos" || echo "Erro ao remover arquivos"
