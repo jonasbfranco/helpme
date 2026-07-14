26ebb27fe27366f01ba93f5fe0a5aaf72df2adcd
Jonas Baptista Franco <jonas@192.168.0.22>
2026-07-13 22:30:41 -0300

    :sparkles:: Rota Deletar Perfil

--- git-commit.sh
+++ git-commit.sh
@@ -5,20 +5,28 @@ git status
 
 echo
 echo "Escolha o tipo do commit:"
-echo "1) feat"
-echo "2) fix"
-echo "3) docs"
-echo "4) refactor"
-echo "5) chore"
+echo "1) Initial commit	🎉 :tada:"
+echo "2) Version tag	🔖 :bookmark:"
+echo "3) New feature	✨ :sparkles:"
+echo "4) Bugfix	        🐛 :bug:"
+echo "5) feat"
+echo "6) fix"
+echo "7) docs"
+echo "8) refactor"
+echo "9) chore"
 
 read -p "Opção: " opcao
 
 case $opcao in
-    1) tipo="feat" ;;
-    2) tipo="fix" ;;
-    3) tipo="docs" ;;
-    4) tipo="refactor" ;;
-    5) tipo="chore" ;;
+    1) tipo=":tada:";;
+    2) tipo=":bookmark:";;
+    3) tipo=":sparkles:";;
+    4) tipo=":bug:";;
+    5) tipo="feat" ;;
+    6) tipo="fix" ;;
+    7) tipo="docs" ;;
+    8) tipo="refactor" ;;
+    9) tipo="chore" ;;
     *) tipo="chore" ;;
 esac
 
