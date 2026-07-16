44b5b33bf02efab7c179d83727fce49d54d8259f
jonasbfranco <jonasbfranco@hotmail.com>
2026-07-16 20:12:18 -0300

    Alteracao docker-compose criando migrations

--- subir_docker.sh
+++ subir_docker.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
-# apagar arquivos disco externo quando usado no macOS
+
 docker compose up -d && echo "Docker sendo executado" || echo "Erro ao executar docker"
+
+echo "Aguardando o PostgreSQL..."
+
+until docker compose exec postgres pg_isready -U docker >/dev/null 2>&1; do
+    sleep 2
+done
+
+echo "PostgreSQL pronto."
+
+docker compose exec node npx prisma migrate dev
+
+
+## Ou
+
+## Se você ainda está modelando o banco e não quer criar migrations toda hora:
+
+## ## docker compose exec node npx prisma db push
+
+## Ele apenas sincroniza o banco com o schema.
+
+## Depois gere o client:
+
+##  ## docker compose exec node npx prisma generate
